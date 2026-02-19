c0cfc1b20f0524c8226735baa6f1a5653f8f7c15
Chirag H S <chiraghs08@gmail.com>
2026-02-19 09:13:37 +0530

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.py
@@ -0,0 +1,14 @@
+# Last updated: 19/2/2026, 9:13:37 am
+class MyHashMap:
+
+    def __init__(self):
+        self.map = [-1] * 1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.map[key]
+
+    def remove(self, key: int) -> None:
+        self.map[key] = -1
