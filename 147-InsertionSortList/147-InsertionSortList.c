fbf9f3822a6336d5c7e1e1aefb8c66b046723be5
Chirag H S <chiraghs08@gmail.com>
2026-02-19 09:14:14 +0530

    Create 147-InsertionSortList

---
+++ 147-InsertionSortList/147-InsertionSortList.c
@@ -0,0 +1,44 @@
+// Last updated: 19/2/2026, 9:14:14 am
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ void swap(struct ListNode *p1,struct ListNode *p2)
+ {
+     int t=p1->val;
+     p1->val=p2->val;
+     p2->val=t;
+ }
+ int len(struct ListNode *head)
+ {
+     struct ListNode *p=head;
+     int l=0;
+     while(p!=NULL)
+     {
+         l++;
+         p=p->next;
+     }
+     return l;
+ }
+struct ListNode* insertionSortList(struct ListNode* head){
+    struct ListNode *p=head;
+    int i,j;
+    int l=len(head);
+    for(i=0;i<l;i++)
+    {
+        for(j=0;j<l-i-1;j++)
+        {
+            if(p->val>p->next->val)
+            {
+                swap(p,p->next);
+            }
+            p=p->next;
+        }
+        p=head;
+    }
+    return head;
+
+}
