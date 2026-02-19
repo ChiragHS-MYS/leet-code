b724e5512de76b8d7433c7df6a8224c30c353861
Chirag H S <chiraghs08@gmail.com>
2026-02-19 09:14:20 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,8 @@
+# Last updated: 19/2/2026, 9:14:19 am
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
