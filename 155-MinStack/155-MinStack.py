dbfc536d8fb9a9b11f546bfcfa36ce74e992d35a
Chirag H S <chiraghs08@gmail.com>
2026-02-19 09:14:12 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.py
@@ -0,0 +1,20 @@
+# Last updated: 19/2/2026, 9:14:11 am
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
