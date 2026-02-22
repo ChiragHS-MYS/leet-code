783188d1b946f70bfd3ab7bf675a76ec1afb493e
Chirag H S <chiraghs08@gmail.com>
2026-02-22 22:20:23 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -0,0 +1,21 @@
+# Last updated: 22/2/2026, 10:20:23 pm
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
