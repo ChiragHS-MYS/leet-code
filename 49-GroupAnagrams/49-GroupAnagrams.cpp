327b11f1bf3e8af76df24d45240ceb69673efbfb
Chirag H S <chiraghs08@gmail.com>
2026-03-07 23:36:56 +0530

    File updated at 7/3/2026, 11:36:57 pm

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,17 +1,17 @@
-// Last updated: 22/2/2026, 10:20:22 pm
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for (string s : strs) {
-            string t = s; 
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
-        }
-        vector<vector<string>> anagrams;
-        for (auto p : mp) { 
-            anagrams.push_back(p.second);
-        }
-        return anagrams;
-    }
-};
+// Last updated: 7/3/2026, 11:36:57 pm
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        unordered_map<string, vector<string>> mp;
+5        for (string s : strs) {
+6            string t = s; 
+7            sort(t.begin(), t.end());
+8            mp[t].push_back(s);
+9        }
+10        vector<vector<string>> anagrams;
+11        for (auto p : mp) { 
+12            anagrams.push_back(p.second);
+13        }
+14        return anagrams;
+15    }
+16};
