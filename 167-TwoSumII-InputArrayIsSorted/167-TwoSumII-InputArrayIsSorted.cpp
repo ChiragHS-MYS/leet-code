c7590e4a9fd10c84d5ba11bc28aef5669fe05db8
Chirag H S <chiraghs08@gmail.com>
2026-06-11 22:21:35 +0530

    File updated at 11/6/2026, 10:21:35 pm

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.cpp
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.cpp
@@ -1,23 +1,15 @@
-// Last updated: 11/6/2026, 10:19:08 pm
+// Last updated: 11/6/2026, 10:21:35 pm
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& numbers, int target) {
-4        int r = 0;
-5        int l = numbers.size() - 1;
-6
-7        while (r < l) {
-8            int sum = numbers[r] + numbers[l];
-9
-10            if (sum == target) {
-11                return {r+1, l+1};  // if 1-based needed → {r+1, l+1}
-12            }
-13            else if (sum < target) {
-14                r++;
-15            }
-16            else {
-17                l--;  // ✅ FIXED
-18            }
-19        }
-20        return {-1, -1};
-21    }
-22};
+4        unordered_map<int,int>mp;
+5        for(int i=0;i<numbers.size();i++){
+6            int value=target-numbers[i];
+7            if(mp.find(value)!=mp.end()){
+8                return {mp[value]+1,i+1};
+9            }
+10            mp[numbers[i]]=i;
+11        }
+12        return {};
+13    }
+14};
