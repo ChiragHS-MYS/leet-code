2b4f79d512af31ba3c55f39681df887385fda399
Chirag H S <chiraghs08@gmail.com>
2026-07-05 14:21:56 +0530

    Create

---
+++ problem-1783241515923/problem-1783241515923.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/7/2026, 2:21:55 pm
+1class Solution {
+2public:
+3    int climbStairs(int n) {
+4        if(n==1)return 1;
+5        if (n==2) return 2;
+6        int prev1=1;
+7        int prev2=2;
+8        long cur;
+9        for(int i=3;i<=n;i++){
+10            cur= prev1+prev2;
+11            prev1=prev2;
+12            prev2=cur;
+13        }
+14        return prev2;
+15        
+16    }
+17};
