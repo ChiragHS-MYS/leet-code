e5e058787cf6cd5ec8d7dd63e2c5ae9e78314330
Chirag H S <chiraghs08@gmail.com>
2026-07-08 20:51:49 +0530

    File updated at 8/7/2026, 8:51:48 pm

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,13 +1,21 @@
-// Last updated: 8/7/2026, 8:49:35 pm
+// Last updated: 8/7/2026, 8:51:48 pm
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4        unordered_map<int,int>mp;
-5        for(int i:nums){
-6            mp[i]++;
-7            if(mp[i]>(nums.size()/2))
-8                return i;
-9        }
-10        return 0;
-11    }
-12};
+4        int candidate=0;
+5        int count=0;
+6        for(int i=0;i<nums.size();i++){
+7            if(count==0){
+8                candidate=nums[i];
+9            }
+10            if(nums[i]==candidate){
+11                count++;
+12            }
+13            else{
+14                count--;
+15            }
+16        }
+17        return candidate;
+18        
+19    }
+20};
