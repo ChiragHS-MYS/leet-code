01b599106d4fba978c9a361e6f9cd2774ece4ccc
Chirag H S <chiraghs08@gmail.com>
2026-07-09 09:13:57 +0530

    File updated at 9/7/2026, 9:13:55 am

--- 278-FirstBadVersion/278-FirstBadVersion.cpp
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -1,24 +1,22 @@
-// Last updated: 9/7/2026, 9:12:50 am
-1// The API isBadVersion is defined for you.
-2// bool isBadVersion(int version);
-3
-4class Solution {
-5public:
-6    int firstBadVersion(int n) {
-7        int left=0;
-8        int right=n;
-9        int ans=-1;
-10        int mid;
-11        while(left<=right){
-12            mid=left+(right-left)/2;
-13            if(isBadVersion(mid)== true){
-14                ans=mid;
-15                right=mid-1;
-16            }
-17            else if(isBadVersion(mid)== false)
-18                left=mid+1;
-19        
-20        }
-21        return ans;      
-22    }
-23};
+// Last updated: 9/7/2026, 9:13:55 am
+ class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=1,high=n;
+        int ans;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(isBadVersion(mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
