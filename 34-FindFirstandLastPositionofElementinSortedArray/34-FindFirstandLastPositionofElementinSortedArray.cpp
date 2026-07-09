e5ff6886f017f2b136666904b2169950aaf277b4
Chirag H S <chiraghs08@gmail.com>
2026-07-09 09:05:33 +0530

    File updated at 9/7/2026, 9:05:31 am

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.cpp
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.cpp
@@ -1,56 +1,42 @@
-// Last updated: 8/7/2026, 11:22:24 pm
+// Last updated: 9/7/2026, 9:05:31 am
 1class Solution {
 2public:
-3
-4    int firstOccurrence(vector<int>& nums, int target) {
-5        int left = 0;
-6        int right = nums.size() - 1;
-7        int ans = -1;
-8
-9        while (left <= right) {
-10            int mid = left + (right - left) / 2;
-11
-12            if (nums[mid] == target) {
-13                ans = mid;
-14                right = mid - 1;   // Search left
-15            }
-16            else if (nums[mid] < target) {
-17                left = mid + 1;
-18            }
-19            else {
-20                right = mid - 1;
-21            }
-22        }
-23
-24        return ans;
-25    }
-26
-27    int lastOccurrence(vector<int>& nums, int target) {
-28        int left = 0;
-29        int right = nums.size() - 1;
-30        int ans = -1;
-31
-32        while (left <= right) {
-33            int mid = left + (right - left) / 2;
-34
-35            if (nums[mid] == target) {
-36                ans = mid;
-37                left = mid + 1;    // Search right
-38            }
-39            else if (nums[mid] < target) {
-40                left = mid + 1;
-41            }
-42            else {
-43                right = mid - 1;
-44            }
-45        }
-46
-47        return ans;
-48    }
-49
-50    vector<int> searchRange(vector<int>& nums, int target) {
-51
-52        return {firstOccurrence(nums, target),
-53                lastOccurrence(nums, target)};
-54    }
-55};
+3    vector<int> searchRange(vector<int>& nums, int target) {
+4        int left=0;
+5        int right=nums.size()-1;
+6        int ans1=-1;
+7        int mid;
+8        while(left<=right){
+9            mid=left+(right-left)/2;
+10            if(nums[mid]==target){
+11                ans1=mid;
+12                right=mid-1;
+13            }
+14            else if(nums[mid]>target){
+15                right=mid-1;
+16            }
+17            else{
+18                left=mid+1;
+19            }
+20        }
+21        left=0;
+22        right=nums.size()-1;
+23        int ans2=-1;
+24        while(left<=right){
+25            mid=left+(right-left)/2;
+26            if(nums[mid]==target){
+27                ans2=mid;
+28                left=mid+1;
+29            }
+30            else if(nums[mid]>target){
+31                right=mid-1;
+32            }
+33            else{
+34                left=mid+1;
+35            }
+36        }
+37        return{ans1,ans2};
+38        
+39        
+40    }
+41};
