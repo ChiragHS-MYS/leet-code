12f9b71ce4978dc2d50e39d99e47db29c1bfa46d
Chirag H S <chiraghs08@gmail.com>
2026-07-15 13:59:08 +0530

    File updated at 15/7/2026, 1:59:06 pm

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,20 +1,36 @@
-// Last updated: 15/7/2026, 12:43:26 pm
+// Last updated: 15/7/2026, 1:59:06 pm
 1class Solution {
 2public:
-3    int rob(vector<int>& nums) {
-4
-5        int n = nums.size();
+3
+4    int solve(vector<vector<int>>& grid, int i, int j,
+5              vector<vector<int>>& dp) {
 6
-7        if(n == 1)
-8            return nums[0];
+7        int m = grid.size();
+8        int n = grid[0].size();
 9
-10        vector<int> dp(n + 2, 0);
-11
-12        for(int i = n - 1; i >= 0; i--) {
-13            dp[i] = max(nums[i] + dp[i + 2],
-14                        dp[i + 1]);
-15        }
-16
-17        return dp[0];
-18    }
-19};
+10        if(i >= m || j >= n)
+11            return INT_MAX;
+12
+13        if(i == m-1 && j == n-1)
+14            return grid[i][j];
+15
+16        if(dp[i][j] != -1)
+17            return dp[i][j];
+18
+19        int down = solve(grid, i+1, j, dp);
+20        int right = solve(grid, i, j+1, dp);
+21
+22        return dp[i][j] = grid[i][j] + min(down, right);
+23    }
+24
+25    int minPathSum(vector<vector<int>>& grid) {
+26
+27        int m = grid.size();
+28        int n = grid[0].size();
+29
+30        vector<vector<int>> dp(m, vector<int>(n, -1));
+31
+32        return solve(grid, 0, 0, dp);
+33
+34    }
+35};
