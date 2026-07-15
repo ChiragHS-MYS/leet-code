78da6748b007e95c6daa63f385e350c75f855d4e
Chirag H S <chiraghs08@gmail.com>
2026-07-15 14:25:34 +0530

    File updated at 15/7/2026, 2:25:33 pm

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,36 +1,35 @@
-// Last updated: 15/7/2026, 1:59:06 pm
+// Last updated: 15/7/2026, 2:25:33 pm
 1class Solution {
 2public:
-3
-4    int solve(vector<vector<int>>& grid, int i, int j,
-5              vector<vector<int>>& dp) {
-6
-7        int m = grid.size();
-8        int n = grid[0].size();
-9
-10        if(i >= m || j >= n)
-11            return INT_MAX;
-12
-13        if(i == m-1 && j == n-1)
-14            return grid[i][j];
-15
-16        if(dp[i][j] != -1)
-17            return dp[i][j];
-18
-19        int down = solve(grid, i+1, j, dp);
-20        int right = solve(grid, i, j+1, dp);
+3    int solve(string word1,string word2,int i,int j,vector<vector<int>>&dp){
+4        //delete the remaining elements
+5        if(i==word1.size()){
+6            return word2.size()-j;
+7        }
+8        // if add rem letters in word2 as for insert 1
+9        if(j==word2.size()){
+10            return word1.size()-i;
+11        }
+12        if(dp[i][j] != -1)
+13            return dp[i][j];
+14
+15        if(word1[i] == word2[j])
+16            return dp[i][j] = solve(word1, word2, i + 1, j + 1, dp);
+17
+18        int insert = 1 + solve(word1, word2, i, j + 1, dp);
+19
+20        int del = 1 + solve(word1, word2, i + 1, j, dp);
 21
-22        return dp[i][j] = grid[i][j] + min(down, right);
-23    }
-24
-25    int minPathSum(vector<vector<int>>& grid) {
+22        int replace = 1 + solve(word1, word2, i + 1, j + 1, dp);
+23
+24        return dp[i][j] = min(insert, min(del, replace));
+25    }
 26
-27        int m = grid.size();
-28        int n = grid[0].size();
-29
-30        vector<vector<int>> dp(m, vector<int>(n, -1));
+27    int minDistance(string word1, string word2) {
+28
+29        vector<vector<int>> dp(word1.size(),
+30                               vector<int>(word2.size(), -1));
 31
-32        return solve(grid, 0, 0, dp);
-33
-34    }
-35};
+32        return solve(word1, word2, 0, 0, dp);
+33    }
+34};
