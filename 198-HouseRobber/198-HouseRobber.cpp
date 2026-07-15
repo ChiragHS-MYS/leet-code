fa3a197b71bfa98c0547e794e8171ff3687be194
Chirag H S <chiraghs08@gmail.com>
2026-07-15 12:43:27 +0530

    File updated at 15/7/2026, 12:43:26 pm

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,21 +1,20 @@
-// Last updated: 15/7/2026, 12:35:57 pm
+// Last updated: 15/7/2026, 12:43:26 pm
 1class Solution {
 2public:
-3    int findmaxamount(vector<int>& nums,int index,vector<int>&dp){
-4        if(index>=nums.size()){
-5            return 0;
-6        }
-7        if(dp[index]!=-1){
-8            return dp[index];
-9        }
-10        int take=nums[index]+findmaxamount(nums,index+2,dp);
-11        int nottake=findmaxamount(nums,index+1,dp);
-12        dp[index]=max(take,nottake);
-13        return dp[index];
-14    }
-15    int rob(vector<int>& nums) {
-16        vector<int> dp(nums.size(), -1);
-17        return findmaxamount(nums,0,dp);
-18        
-19    }
-20};
+3    int rob(vector<int>& nums) {
+4
+5        int n = nums.size();
+6
+7        if(n == 1)
+8            return nums[0];
+9
+10        vector<int> dp(n + 2, 0);
+11
+12        for(int i = n - 1; i >= 0; i--) {
+13            dp[i] = max(nums[i] + dp[i + 2],
+14                        dp[i + 1]);
+15        }
+16
+17        return dp[0];
+18    }
+19};
