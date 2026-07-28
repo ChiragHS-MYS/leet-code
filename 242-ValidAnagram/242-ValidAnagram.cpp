755c20313e3e443898769da6eef26605ca38a911
Chirag H S <chiraghs08@gmail.com>
2026-07-28 18:13:00 +0530

    File updated at 28/7/2026, 6:13:00 pm

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,15 +1,24 @@
-// Last updated: 8/7/2026, 8:58:48 pm
+// Last updated: 28/7/2026, 6:13:00 pm
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        sort(s.begin(),s.end());
-5        sort(t.begin(),t.end());
-6        if(s==t){
-7            return true;
-8        }
-9        else{
-10            return false;
-11        }
-12        
-13    }
-14};
+4
+5        if(s.length() != t.length())
+6            return false;
+7
+8        unordered_map<char,int> mp;
+9
+10        for(char c : s)
+11            mp[c]++;
+12
+13        for(char c : t){
+14
+15            mp[c]--;
+16
+17            if(mp[c] < 0)
+18                return false;
+19        }
+20
+21        return true;
+22    }
+23};
