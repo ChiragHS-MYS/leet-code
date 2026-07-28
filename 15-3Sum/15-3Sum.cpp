a46f9405f1ccb3429b140e818bd364d67d005f16
Chirag H S <chiraghs08@gmail.com>
2026-07-28 13:10:54 +0530

    File updated at 28/7/2026, 1:10:54 pm

--- 15-3Sum/15-3Sum.cpp
+++ 15-3Sum/15-3Sum.cpp
@@ -1,41 +1,34 @@
-// Last updated: 28/7/2026, 1:05:28 pm
+// Last updated: 28/7/2026, 1:10:54 pm
 1class Solution {
 2public:
 3    vector<vector<int>> threeSum(vector<int>& nums) {
-4        vector<vector<int>> res;
-5        sort(nums.begin(), nums.end());
-6
-7        int n = nums.size();
-8
-9        for (int i = 0; i < n; i++) {
-10
-11            // Skip duplicates
-12            if (i > 0 && nums[i] == nums[i - 1]) continue;
-13
-14            int r = i + 1;
-15            int l = n - 1;
-16
-17            while (r < l) {
-18                int sum = nums[i] + nums[r] + nums[l];
-19
-20                if (sum == 0) {
-21                    res.push_back({nums[i], nums[r], nums[l]});
-22
-23                    // Skip duplicates
-24                    while (r < l && nums[r] == nums[r + 1]) r++;
-25                    while (r < l && nums[l] == nums[l - 1]) l--;
-26
-27                    r++;
-28                    l--;
-29                }
-30                else if (sum < 0) {
-31                    r++;
-32                }
-33                else {
-34                    l--;
-35                }
-36            }
-37        }
-38        return res;
-39    }
-40};
+4        sort(nums.begin(),nums.end());
+5        int low,high,sum;
+6        vector<vector<int>>res;
+7        for(int i=0;i<nums.size();i++){
+8            if(i>0 && nums[i]==nums[i-1]){
+9                continue;
+10            }
+11            low=i+1;
+12            high=nums.size()-1;
+13            while(low<high){
+14                sum=nums[i]+nums[low]+nums[high];
+15                if(sum==0){
+16                    res.push_back({nums[i],nums[low],nums[high]});
+17                    while(low<high && nums[low]==nums[low+1])low++;
+18                    while(low<high && nums[high]==nums[high-1])high--;
+19                    low++;
+20                    high--;
+21                }
+22                else if(sum <0){
+23                    low++;
+24                }
+25                else{
+26                    high--;
+27                }
+28            }
+29
+30        }
+31           return res;
+32    }
+33};
