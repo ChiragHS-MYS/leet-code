0d79fb54b0e4d5ed129d51dffc95ac18b6935b5e
Chirag H S <chiraghs08@gmail.com>
2026-07-28 14:08:11 +0530

    File updated at 28/7/2026, 2:08:11 pm

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,20 +1,20 @@
-// Last updated: 22/2/2026, 10:20:39 pm
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int maxLength = 0;
-        int left = 0;
-        unordered_map<char, int> lastSeen;
-
-        for (int right = 0; right < s.length(); right++) {
-            char c = s[right];
-            if (lastSeen.find(c) != lastSeen.end() && lastSeen[c] >= left) {
-                left = lastSeen[c] + 1;
-            }
-            maxLength = max(maxLength, right - left + 1);
-            lastSeen[c] = right;
-        }
-
-        return maxLength;        
-    }
-};
+// Last updated: 28/7/2026, 2:08:11 pm
+1class Solution {
+2public:
+3    int lengthOfLongestSubstring(string s) {
+4        int maxLength = 0;
+5        int left = 0;
+6        unordered_map<char, int> lastSeen;
+7
+8        for (int right = 0; right < s.length(); right++) {
+9            char c = s[right];
+10            if (lastSeen.find(c) != lastSeen.end() && lastSeen[c] >= left) {
+11                left = lastSeen[c] + 1;
+12            }
+13            maxLength = max(maxLength, right - left + 1);
+14            lastSeen[c] = right;
+15        }
+16
+17        return maxLength;        
+18    }
+19};
